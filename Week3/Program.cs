28d8a189a91a5ac7c8ff9224cc9fe9dff6e82848
Dre <aciobirca97@gmail.com>
2024-01-07 21:48:08 +0000

    first commit

---
+++ Week3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Shapes;
+
+interface IHasVolume
+{
+    double HasVolume();
+}
+
+public class Sphere : IHasVolume
+{
+    //field to store radius
+    private double radius;
+
+    //Making sure Radius is postive
+    public double Radius
+    {
+        get { return radius; }
+        set
+        {
+            if (value > 0)
+                radius = value;
+            else
+                throw new ArgumentException("Radius must be positive.");
+        }
+    }
+
+    public Sphere(double radius)
+    {
+        this.Radius = radius;
+    }
+
+
+
+    //overrides volume
+    public double HasVolume()
+    {
+        return Math.PI * radius * radius * radius;
+    }
+
+
+    //Creates constructor in Sphere 
+  
+    public override string ToString()
+    {
+        return  "Radius = " + radius + " \nCalculate volume =  " + HasVolume();
+    }
+
+}
+
---
+++ Week3/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Shapes;
+
+class Test
+{
+    public static void Main()
+    {
+
+        var spheres = new List<Sphere>
+        {
+            new (4),
+            new (5),
+            new (69)
+        };
+        foreach (Sphere s in spheres)
+        {
+            Console.WriteLine(s.ToString());
+        }
+       
+    }
+}
